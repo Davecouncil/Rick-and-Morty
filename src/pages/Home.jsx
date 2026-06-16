f593b77dd95e955f989de057456e0fc8ef7b4aed
Davecouncil <councildave2@gmail.com>
2026-06-16 16:25:51 +0100

    a new div in the button for favorite chracters almost done sha

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,6 +45,7 @@
 // }
 import React from 'react'
 import { Link } from 'react-router'
+import { RiArrowRightCircleLine } from "react-icons/ri";
 import ChatGpt from '../assets/ChatGPT Image Jun 16, 2026, 02_37_44 PM.png'
 
 
@@ -57,15 +58,26 @@ export default function Home() {
 
       <div className='absolute w-screen text-white h-120 flex justify-center items-center '>
         <div className='flex flex-col gap-7 items-center'>
-      <h1 className='text-6xl font-bold '>Explore the Multiverse</h1>
-      <p className='text-wrap text-xl text-gray-200 '>Browse hundreds of characters from across every dimension <br /> of the Rick and Morty universe. Wubba lubba dub dub!</p>
-      <Link to="/characters" className=' bg-[#79D36E] px-7 py-3 rounded-xl '> Browse Characters </Link>
+      <h1 className='text-4xl md:text-5xl lg:text-6xl font-extrabold text-white leading-tight max-w-3xl '>Explore the Multiverse</h1>
+      <p className='text-wrap mt-6 text-lg md:text-xl text-gray-200 max-w-xl leading-relaxed '>Browse hundreds of characters from across every dimension of the Rick and Morty universe. Wubba lubba dub dub!</p>
+      <Link to="/characters" className=' bg-[#79D36E] mt-10 inline-block px-7 py-3 rounded-xl  text-base font-semibold text-white rounded-md hover:bg-green-800 transition-colors'> Browse Characters </Link>
       </div>
     </div>
     </div>
-    <div>
 
+
+    <section className="bg-gray-100 py-20">
+      <div className="max-w-7xl mx-auto px-6">
+    <div className='flex flex-col items-center gap-4 '>
+      <h1>Featured Characters</h1>
+      <p className='flex w-120 items-center justify-cente '>Meet some of the most iconic faces from across the Central Finite Curve.</p>
+      <div>
+        hhhh
+      </div>
+      <Link> View All Character <RiArrowRightCircleLine /> </Link>
     </div>
+    </div>
+    </section>
     </>
   )
 }
